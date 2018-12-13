ddf66e2b6798ff5bba82593b044df5e332707d37
rezwana rubaiya <rezwanarubaiya@gmail.com>
2018-12-12 22:28:57 -0600

    update

--- README.md
+++ README.md
@@ -1,1 +1,47 @@
-# Psychic-Game
+# Psychic Game
+
+Overview
+In this assignment, you'll create one of two possible computer games: Word Guess or Psychic. These apps will run in the browser, and feature dynamically updated HTML and CSS powered by your JavaScript code.
+
+
+Task:
+
+1.Create a new GitHub repo called Psychic-Game, in accordance with the assignment you choose to complete. Then, clone it to your computer.
+
+2.Inside your local git repository, create an index.html.
+
+3.While still in your local git repo, create a directory called assets.
+
+4.cd your way into the assets folder, then make three additional folders: javascript, css and images.
+
+5.In the javascript folder, make a file called game.js. Use the src attribute of the script tag to link to this file, rather than embedding the code directly in your HTML document.
+In the css folder, make a file called style.css.
+Also in the css folder, make a file called reset.css. Paste into it the code from the Meyerweb reset stylesheet. If you opt to use Bootstrap instead of writing your own CSS, skip this step, and simply include a link to Bootstrap via CDN.
+In the images folder, save whatever images you plan on using.
+├── assets
+|  ├── css
+|  |  └── style.css
+|  ├── images
+|  └── javascript
+|     └── game.js
+└── index.html
+Push the above changes to GitHub.
+
+
+
+Put the following text on your page:
+
+
+Guess what letter I'm thinking of
+
+Wins: (# of times the user has guessed the letter correctly)
+
+Losses: (# of times the user has failed to guess the letter correctly after exhausting all guesses)
+
+Guesses Left: (# of guesses left. This will update)
+
+Your Guesses So Far: (the specific letters that the user typed. Display these until the user either wins or loses.)
+
+When the player wins, increase the Wins counter and start the game over again (without refreshing the page).
+
+When the player loses, increase the Losses counter and restart the game without a page refresh (just like when the user wins).
---
+++ assets/js/game.js
@@ -0,0 +1,79 @@
+//all variables...
+var letter = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var wins = 0;
+var losses = 0;
+var guesses = 9;
+var guessesLeft = 9;
+var playerGuessed = [];
+var playerGuess = null;
+var computerGuess = [];
+
+
+// connect to HTML through id to visualization of guessesLeft for player .  
+
+var newGuessesLeft = function () {
+    document.querySelector("#guessLeft").innerHTML = "Guesses Left: " + guessesLeft;
+};
+
+//computer chooses random letter from its choices in the variable at the top. console log chosen letter.
+
+var newplayerGuess = function () {
+    playerGuess = letter[Math.floor(Math.random() * letter.length)];
+    console.log(playerGuess);
+};
+
+//display all keys pressed by player and separate by a comma
+
+var newGuessesSoFar = function () {
+    document.querySelector('#playerGuess').innerHTML = "Your Guesses so far: " + playerGuessed.join(', ');
+};
+
+//reset everything after win or lose
+
+var reset = function () {
+    guessesLeft = 9;
+    guessedLetters = [];
+
+    newGuessesLeft();
+    newplayerGuess();
+    newGuessesSoFar();
+}
+
+// When the player presses a key, it will run the following function...
+
+document.onkeyup = function (event) {
+
+//remaining guesses
+
+    guessesLeft--;
+
+    console.log(event.key);
+    console.log(playerGuessed);
+    
+//make player each guess lower case 
+
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+//push the guessed letter to playerGuess, update var functions.
+
+    playerGuessed.push(userGuess);
+    newGuessesLeft();
+    newGuessesSoFar();
+
+//when player still has guesses remaining and get letter, they win. if they have no guesses left, they lose.
+    
+    if (guessesLeft > 0) {
+        if (userGuess == playerGuess) {
+            wins++;
+            document.querySelector('#win').innerHTML = "Wins: " + wins;
+            alert("Wow! You are psychic! Try again!");
+            reset();
+        }
+    } else if (guessesLeft == 0) {
+            losses++;
+            document.querySelector('#loss').innerHTML = "Losses: " + losses;
+            alert("Oops, you're not psychic... try again!");
+            reset();
+    }
+
+};
